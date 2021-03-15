b74d219ce959ed22eaa6d3b796f8e63ab5bc0992
Great Dolphin <great.dolphin.ls@gmail.com>
2021-03-15 06:46:10 -0700

    [feat] remove unused package

--- services/jupiter.js
+++ services/jupiter.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-import { gravity } from '~/libs/gravity';
-
 exports.getAccountIdByPassphrase = async (passphrase) => {
   const url = `${process.env.JUPITERSERVER}/nxt?requestType=getAccountId&secretPhrase=${passphrase}`;
   return await axios.get(url)
